2efbe882ce8930d5cf23fdb96c35fc4611257b58
Lucio Torre <lucio.torre@gmail.com>
2012-07-06 21:38:25 -0300

    bester camper ever.

--- bots/camper.py
+++ bots/camper.py
@@ -8,11 +8,26 @@ import spacecraft
 from spacecraft import euclid
 
 
+def closer(target, candidates):
+    ranked = [(abs(target - p), p) for p in candidates]
+    ranked.sort()
+    return ranked[0][1]
+
+
 class RandomClient(spacecraft.server.ClientBase):
 
     name = "camper"
-    camp = euclid.Point2(4, 4)
+    camp = None
     lookout = euclid.Point2(50, 50)
+    lookout_duration = 1
+
+    path = []
+    old = [
+        euclid.Point2(15.404826, 14.010764),
+        euclid.Point2(79.549515, 17.167492),
+        euclid.Point2(84.600273, 88.00444),
+
+    ]
 
     camp_positions = [
         euclid.Point2(4, 4),
@@ -20,6 +35,20 @@ class RandomClient(spacecraft.server.ClientBase):
         euclid.Point2(4, 95),
         euclid.Point2(95, 4)
         ]
+
+    lookout_positions = [
+        euclid.Point2(40, 60),
+        euclid.Point2(60, 40),
+        euclid.Point2(60, 60),
+        euclid.Point2(40, 40),
+        ]
+
+    def goto(self, target):
+        # aim to corrected goal
+        # if more or less looking
+        # throttle
+        pass
+
     def look_to(self, target):
         turn = relative_angle(self.pos.x, self.pos.y,
             target.x, target.y, self.angle)
@@ -33,7 +62,38 @@ class RandomClient(spacecraft.server.ClientBase):
             self.pos = euclid.Point2(*message['gps']['position'])
             self.angle = message['gps']['angle']
 
+            enemy_found = False
+
+            for obj in message.get('proximity', []):
+                if obj['object_type'] in ['player']:
+                    enemy_found = True
+                    enemy_pos = euclid.Point2(*obj['position'])
+                    enemy_vel = euclid.Point2(*obj['velocity'])
+
+            # if enemy nearby, aim and shoot
+            if enemy_found:
+                #self.camp = random.choice(self.camp_positions)
+                self.look_to(enemy_pos)
+                self.command("throttle", value=0.1)
+                self.command("fire")
+                return
+
+            if not self.camp:
+                # pick closest
+                self.camp = closer(self.pos, self.camp_positions)
+
+            if self.path:
+                dist = abs(self.pos - self.path[0])
+                if dist > 3:
+                    self.look_to(self.path[0])
+                    self.command("throttle", value=1)
+                else:
+                    self.path = self.path[1:]
+                return
+
+
             # if not camped goto camp position
+            print self.camp
             dist = abs(self.pos - self.camp)
             if dist > 10:
                 self.look_to(self.camp)
@@ -43,26 +103,13 @@ class RandomClient(spacecraft.server.ClientBase):
                 self.command("throttle", value=0.1)
             else:
                  # in position
-                enemy_found = False
-
-                for obj in message.get('proximity', []):
-                    if obj['object_type'] in ['player']:
-                        enemy_found = True
-                        enemy_pos = euclid.Point2(*obj['position'])
-                        enemy_vel = euclid.Point2(*obj['velocity'])
-
-                # if enemy nearby, aim and shoot
-                if enemy_found:
-                    dist_enemy = abs(self.pos - enemy_pos)
-                    if dist_enemy < 20:
-                        # if close, flee!
-                        self.camp = random.choice(self.camp_positions)
-                    self.look_to(enemy_pos)
-                    self.command("fire")
-                else:
-                    # else just aim to lookout
-                    self.look_to(self.lookout)
-                    self.command("fire")
+                # else just aim to lookout
+                self.look_to(self.lookout)
+                self.command("fire")
+                if self.lookout_duration < 0.5:
+                    self.lookout = random.choice(self.lookout_positions)
+                    self.lookout_duration = 1
+                self.lookout_duration -= 0.05
 
 
 def main():
